55757a4c3766fb47703461b7913420a918a790f8
pandeytrayambak1519 <pandeytrayambak1519@gmail.com>
2026-08-03 02:32:11 +0530

    Fix CORS

--- src/main/java/com/UserService/UserService/util/CorsConfig.java
+++ src/main/java/com/UserService/UserService/util/CorsConfig.java
@@ -16,10 +16,12 @@ public class CorsConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(
-                List.of("http://localhost:5173", "https://ai-expense-manager-frontend.vercel.app")
+        configuration.setAllowedOriginPatterns(
+                List.of(
+                        "http://localhost:5173",
+                        "https://*.vercel.app"
+                )
         );
-
         configuration.setAllowedMethods(
                 List.of(
                         "GET",
